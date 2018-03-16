a1e0fb0243380ae6fc081ec65494713c1bd3feac
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-03-17 02:56:36 +0530

    speech

--- SchemesForMe/app/src/main/java/dsinghvimegha/schemesforme/MainActivity.java
+++ SchemesForMe/app/src/main/java/dsinghvimegha/schemesforme/MainActivity.java
@@ -1,16 +1,36 @@
 package dsinghvimegha.schemesforme;
 
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
+    TextToSpeech toSpeech;
+    int result;
+    String text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toSpeech = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener(){
+            @Override
+            public void onInit(int i) {
+                result = toSpeech.setLanguage(Locale.UK);
+                }
+        });
+    }
+
+    public void TTS(View view){
+        text = "Hello";
+        toSpeech.speak(text, TextToSpeech.QUEUE_FLUSH,null);
+
     }
     public void english(View view){
         Intent intent = new Intent(this, English1Activity.class);
@@ -26,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void tamil(View view){
         Intent intent = new Intent(this, Tamil1Activity.class);
+
         startActivity(intent);
 
     }
