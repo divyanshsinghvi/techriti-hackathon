b15cbf99c2b11bcfca6cd20f82be760266e8408e
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-03-17 02:06:59 +0530

    second page eenglish

--- SchemesForMe/app/src/main/java/dsinghvimegha/schemesforme/MainActivity.java
+++ SchemesForMe/app/src/main/java/dsinghvimegha/schemesforme/MainActivity.java
@@ -1,5 +1,6 @@
 package dsinghvimegha.schemesforme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,8 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void sendMessage(View view){
-
+    public void english(View view){
+        Intent intent = new Intent(this, English2Activity.class);
+        startActivity(intent);
 
     }
 }
